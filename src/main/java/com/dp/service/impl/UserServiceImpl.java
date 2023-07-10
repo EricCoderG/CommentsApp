6b36851096552b4a1ed7f14755f9cf9150e8f673
Ruilin Geng <1585363499@qq.com>
2023-07-10 11:23:59 +0800

    v1.1 短信验证码登录

--- src/main/java/com/dp/service/impl/UserServiceImpl.java
+++ src/main/java/com/dp/service/impl/UserServiceImpl.java
@@ -1,13 +1,69 @@
 package com.dp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dp.dto.LoginFormDTO;
+import com.dp.dto.Result;
 import com.dp.entity.User;
 import com.dp.mapper.UserMapper;
 import com.dp.service.IUserService;
+import com.dp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.dp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误!");
+        }
+        // 2.生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        // 3.保存验证码到session
+        session.setAttribute("code", code);
+        // 4.发送验证码 (模拟)
+        log.debug("向手机号 {} 发送验证码：{}", phone, code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误!");
+        }
+        // 2.校验验证码
+        String cacheCode = (String) session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            return Result.fail("验证码错误!");
+        }
+        // 3.根据手机号查询用户
+        User user = this.query().eq("phone", phone).one();
+        // 4.如果用户不存在，则注册用户
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+        // 5.保存用户到session
+        session.setAttribute("user", user);
+        return Result.ok(); // 因为是基于session的登录，所以不需要返回登录凭证
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(10));
+        this.save(user);
+        return user;
+    }
 }
