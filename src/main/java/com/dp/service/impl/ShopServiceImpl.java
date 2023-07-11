2da58f6bb5e9808ec3b925e047ebe2916178901f
Ruilin Geng <1585363499@qq.com>
2023-07-11 13:57:47 +0800

    v2.0 添加Redis缓存

--- src/main/java/com/dp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/dp/service/impl/ShopServiceImpl.java
@@ -1,13 +1,42 @@
 package com.dp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.dp.dto.Result;
 import com.dp.entity.Shop;
 import com.dp.mapper.ShopMapper;
 import com.dp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.dp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        // 从Redis中获取商铺信息
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 如果Redis中有数据，直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 如果Redis中没有数据，从数据库中查询
+        Shop shop = getById(id);
+        if (shop == null) {
+            return Result.fail("商铺不存在!");
+        }
+        // 将数据写入Redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
+        return Result.ok(shop);
+    }
 }
