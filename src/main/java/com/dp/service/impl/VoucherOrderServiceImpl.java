92437b99bae42d4c4e12ac2514334f39beefdcee
Ruilin Geng <1585363499@qq.com>
2023-07-20 00:26:25 +0800

    v3.2 一人一单问题(*)

--- src/main/java/com/dp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/dp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.dp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dp.utils.RedisIdWorker;
 import com.dp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +27,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     RedisIdWorker redisIdWorker;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         //查询优惠券
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -42,6 +42,26 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (voucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
+        Long userId = UserHolder.getUser().getId();
+        // 粒度控制到用户级别,并且保证事务在锁被释放的时候已经被提交
+        synchronized (userId.toString().intern()) { // 只要是值相同的对象，都会共用同一把锁
+            // 通过代理调用同一个类中的方法，事务是不会生效的，因为代理类是通过反射调用的
+            // 所以需要通过AopContext.currentProxy()获取代理对象，然后调用代理对象的方法
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Override
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        //一人一单
+        Long userId = UserHolder.getUser().getId();
+        //判断是否已经抢购过
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if (count > 0) {
+            return Result.fail("您已经抢购过了");
+        }
         //扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
@@ -55,11 +75,12 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         VoucherOrder order = new VoucherOrder();
         long orderId = redisIdWorker.nextId("order");
         order.setId(orderId);
-        Long userId = UserHolder.getUser().getId();
+        userId = UserHolder.getUser().getId();
         order.setUserId(userId);
         order.setVoucherId(voucherId);
         save(order);
         //返回订单id
         return Result.ok(orderId);
     }
+
 }
